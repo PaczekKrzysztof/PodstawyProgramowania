e1e719aaf5b564414671bfdb693354e8608ca2a7
krzys <krzys@GAMER-PC>
2025-03-06 21:36:43 +0100

    Dodaj pliki projektów.

---
+++ Nauka/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Nauka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Myint = 2;
+            Console.WriteLine(Myint);
+        }
+    }
+}
---
+++ Zadanie 1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace Zadanie_1 //Napisać program wypisujący na ekranie tekst "Witaj Świecie". 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Witaj Świecie");
+            Console.ReadKey(true);
+        }
+    }
+}
