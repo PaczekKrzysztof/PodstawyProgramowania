ff336cc2c866f704963edd0d78a611576543afd7
PaczekKrzysztof <krzysztofpaczek488@gmail.com>
2025-03-07 14:42:00 +0100

    Zadania1-2

---
+++ Zad1-if/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad1_if
+{
+    class Program
+    {
+        static void Main(string[] args)/*Napisać program wczytujący liczbę całkowitą i wypisujący na ekranie informację czy jest to
+liczba parzysta, czy też nieparzysta*/
+        {
+            int a;
+            Console.WriteLine("Podaj liczbe mordo");
+           a = Convert.ToInt32(Console.ReadLine());
+
+            if (a % 2 == 0)
+                Console.WriteLine("Twoja liczba jest parzysta");
+            else
+                Console.WriteLine("Twoja liczba nie jest parzysta");
+            Console.ReadKey(true);
+        }
+        
+    }
+}
---
+++ Zadanie 2-if/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_2_if
+{
+    class Program
+    {
+        static void Main(string[] args)/*Napisać program wczytujący liczbę całkowitą i wypisujący informację, czy wczytana liczba jest
+dodatnia, ujemna, czy też równa zero.*/
+        {
+            int a;
+            Console.WriteLine("Podaj liczbe mordo");
+            a = Convert.ToInt32(Console.ReadLine());
+            if (a == 0)
+                Console.WriteLine("Twoja liczba jest równa 0");
+            else if (a > 0)
+                Console.WriteLine("Twoja liczba jest dodatnia");
+            else
+                Console.WriteLine("Twoja liczba jest ujemna");
+
+        }
+    }
+}
