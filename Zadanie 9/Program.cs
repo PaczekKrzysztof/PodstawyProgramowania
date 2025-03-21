bbe5266b20067dd15e255e321b4cebf9ee4feb63
PaczekKrzysztof <k.paczek.025@studms.ug.edu.pl>
2025-03-21 22:18:45 +0100

    9 i 10

---
+++ Zadanie 9/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_9
+{
+    class Program
+    {
+        static void Main(string[] args)//Napisać program wczytujący dwie liczby całkowite i wypisujący na ekranie sumę kwadratów wczytanych liczb.
+        {
+            int a, b, kwadrat;
+            Console.WriteLine("Podaj dwie liczby");
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            kwadrat = (a * a) + (b * b);
+            Console.WriteLine("Suma kwadratów tych liczb wynosi {0}", kwadrat);
+            Console.ReadKey(true);
+        }
+    }
+}
