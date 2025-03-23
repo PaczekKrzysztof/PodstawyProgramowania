3c8e782383dfc955e9cde871707beacf0b4e1905
PaczekKrzysztof <k.paczek.025@studms.ug.edu.pl>
2025-03-23 19:46:11 +0100

    Całe zadanie 1 zrobione

---
+++ Zadanie 11/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_11
+{
+    class Program
+    {
+        static void Main(string[] args)//Napisać program wczytujący długości boków prostopadłościanu i wypisujący na ekranie jego objętość i pole całkowite. 
+        {
+            double a, b, c, V, Pc;
+            Console.WriteLine("Podaj 3 długości boków prostopadłościanu");
+            a = Convert.ToDouble(Console.ReadLine());
+            b = Convert.ToDouble(Console.ReadLine());
+            c = Convert.ToDouble(Console.ReadLine());
+            V = a * b * c;
+            Pc = 2 * (a * b) + 2 * (a * c) + 2 * (b * c);
+            Console.WriteLine("Objętość wynosi {0}, a pole całkowite {1}", V, Pc);
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ Zadanie10/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie10
+{
+    class Program
+    {
+        static void Main(string[] args)// Napisać program wczytujący promień koła i wypisujący na ekranie jego pole i obwód.
+        {
+            double r,P,obw;
+            Console.WriteLine("Podaj długość promienia");
+            r = Convert.ToDouble(Console.ReadLine());
+            P = Math.PI * r * r;
+            obw = Math.PI * r * 2;
+            Console.WriteLine("Pole tego koła wynosi {0}, a obwód {1}", P, obw);
+            Console.ReadKey(true);
+            
+
+        }
+    }
+}
