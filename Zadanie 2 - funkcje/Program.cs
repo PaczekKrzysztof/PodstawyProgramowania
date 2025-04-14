45f7e073a4c62520f96684c05080eef78d8f1738
Krzysztof Pączek <k.paczek.025@studms.ug.edu.pl>
2025-04-14 13:53:38 +0200

    Do 3 zrobione

---
+++ Zadanie 2 - funkcje/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_2___funkcje
+{
+    internal class Program/*Napisz funkcję o nazwie max, która wyznacza maksimum z dwóch liczb rzeczywistych
+zadanych jako argumenty tej funkcji.Napisz program wykorzystujący funkcję max.*/
+    {
+        static int max(int a, int b)
+        { if (a > b) return a;
+            else return b;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Z liczb 69 i 420 większa jest {0}", max(69, 420));
+            Console.WriteLine("Z liczb 69 i 420 większa jest {0}", max(420, 69));
+            Console.WriteLine("Z liczb 69 i 420 większa jest {0}", max(69, 69));
+            Console.ReadKey(true);
+        }
+
+
+
+    }
+}
---
+++ Zadanie 3 - funkcje/((.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_3___funkcje
+{
+    internal class Program
+    {
+        static double polekola(double r)
+        {
+            return (Math.PI * r * r);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pole kola o promieniu 7 wynosi {0}", polekola(7));
+            Console.ReadKey(true);
+        }
+
+
+    }
+}
