2e8ea6664d1353aced5f2ee248af3bc3b3afdbe5
Krzysztof Pączek <k.paczek.025@studms.ug.edu.pl>
2025-05-12 14:18:38 +0200

    Koniec

---
+++ Zadanie 4 - funkcje/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_4___funkcje
+{
+    internal class Program/*Napisz funkcję o nazwie ile_cyfr, która wyznacza liczbę cyfr dla zadanej liczby naturalnej jako
+argument tej funkcji. Napisz program wykorzystujący funkcję ile_cyfr.*/
+    {
+        static int ile_cyfr(int liczba)
+     
+        {
+            int i = 0;
+            while (liczba / 10 > 0)
+            {
+                liczba /= 10;
+                i++;
+            }
+            return i + 1;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Liczba 0 ma {0} cyfrę", ile_cyfr(0));
+            Console.WriteLine("Liczba 2137 ma {0} cyfr", ile_cyfr(2137));
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ Zadanie 5 - funkcje/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_5___funkcje
+{
+    internal class Program/*Napisz funkcję, która sprawdza czy jej argument jest liczbą pierwszą. Jako wartość funkcja
+powinna zwracać 1 dla liczb pierwszych i 0 dla pozostałych liczb. Przetestuj działanie
+utworzonej funkcji.*/
+    {
+        static int czy_pierwsza(int liczba)
+        {
+            int i;
+            for (i = 2; i < liczba / 2; i++)
+                if (liczba % i == 0) return 0;
+            return 1;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Czy 69 jest liczbą pierwszą?; {0}", czy_pierwsza(69));
+            Console.WriteLine("Czy 5 jest liczba pierwzą?: {0}", czy_pierwsza(5));
+            Console.ReadKey(true);
+
+        }
+    }
+}
